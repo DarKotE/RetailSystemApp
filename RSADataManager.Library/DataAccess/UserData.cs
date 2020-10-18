f42f050ea4195f63c6400bde6f7b60b474fefce7
yamierite <darkote666@gmail.com>
2020-10-19 00:44:04 +0300

    Create class library for data

---
+++ RSADataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSADataManager.Library.Internal.DataAccess;
+using RSADataManager.Library.Models;
+
+namespace RSADataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            var sqlAccess = new SqlDataAccess();
+            var p = new { Id = Id };
+            return sqlAccess.LoadData<UserModel, dynamic>("[dbo].[spUserLookup]", p, "RSAData");
+        }
+    }
+}
