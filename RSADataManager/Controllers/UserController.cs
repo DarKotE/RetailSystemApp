c3fc25c44de871678a7df1d4abd936eca516d5e8
yamierite <darkote666@gmail.com>
2020-10-19 00:44:36 +0300

    Add user api controller

---
+++ RSADataManager/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using RSADataManager.Library.DataAccess;
+using RSADataManager.Library.Models;
+
+namespace RSADataManager.Controllers
+{
+    [Authorize]    
+    public class UserController : ApiController
+    {
+        
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            return new UserData().GetUserById(userId);
+        }
+
+    }
+}
