25aaca9e6c23955a5a9a0189bb84e2eaa7598b31
yamierite <darkote666@gmail.com>
2020-10-19 03:19:16 +0300

    Design initial template for Sales page(view+viewmodel)

---
+++ RSADesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace RSADesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+        public string Tax
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+        public string Total
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+
+
+        private string _itemQuantity;
+        private readonly string _subTotal;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public bool CanAddToCart => true;
+            //UserName?.Length > 0 && Password?.Length > 0;        
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart => true;
+            //UserName?.Length > 0 && Password?.Length > 0;
+        public void RemoveFromCart()
+        {
+
+        }
+        public bool CanCheckOut => true;
+    //UserName?.Length > 0 && Password?.Length > 0;
+        public void CheckOut()
+        {
+
+        }
+
+
+    }
+}
