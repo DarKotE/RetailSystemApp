ca1679b3f16515eb1f86aef67f2535cac6e66d84
yamierite <DarKotE@users.noreply.github.com>
2020-10-25 08:32:48 +0300

    Updated shell view model to have log out and exit functionality

--- RSADesktopUI.Library/Models/LoggedInUserModel.cs
+++ RSADesktopUI.Library/Models/LoggedInUserModel.cs
@@ -14,5 +14,15 @@ namespace RSADesktopUI.Library.Models
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public DateTime CreatedDate { get; set; }
+
+        public void Clear()
+        {
+            Token = String.Empty;
+            Id = String.Empty;
+            FirstName = String.Empty;
+            LastName = String.Empty;
+            EmailAddress = String.Empty;
+            CreatedDate = DateTime.MinValue;
+        }
     }
 }
--- RSADesktopUI/ViewModels/ShellViewModel.cs
+++ RSADesktopUI/ViewModels/ShellViewModel.cs
@@ -5,15 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
 using RSADesktopUI.EventModels;
+using RSADesktopUI.Library.Models;
 
 namespace RSADesktopUI.ViewModels
 {
     public class ShellViewModel: Conductor<object>, IHandle<LogOnEventModel>
     {
         private SalesViewModel _salesVM;
+        private ILoggedInUserModel _user;
         private IEventAggregator _events;
 
         public ShellViewModel(SalesViewModel salesVM,
+                              ILoggedInUserModel user,
                               IEventAggregator events)
         {
             //start listening to events
@@ -21,15 +24,31 @@ namespace RSADesktopUI.ViewModels
             _events.Subscribe(subscriber: this);
 
             _salesVM = salesVM;
+            _user = user;
 
             //get fresh login instance
             ActivateItem(IoC.Get<LoginViewModel>());
+        }
+
+        public bool IsLoggedIn => !String.IsNullOrWhiteSpace(_user.Token);
+
+        public void ExitApp()
+        {
+            TryClose();
+        }
+
+        public void LogOut()
+        {
+            _user.Clear();
+            ActivateItem(IoC.Get<LoginViewModel>());
+            NotifyOfPropertyChange(() => IsLoggedIn);
         }
 
         public void Handle(LogOnEventModel message)
         {
             //redirect to sales page
             ActivateItem(_salesVM);
+            NotifyOfPropertyChange(() => IsLoggedIn);
         }
     }
 }
