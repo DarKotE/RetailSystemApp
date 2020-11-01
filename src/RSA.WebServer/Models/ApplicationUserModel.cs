5f40868c9a3540b14e22c9819a2e4e27e3483626
yamierite <DarKotE@users.noreply.github.com>
2020-11-01 06:54:24 +0300

    Add ctor to app user model Should be removed in c# 9.0

--- src/RSA.WebServer/Models/ApplicationUserModel.cs
+++ src/RSA.WebServer/Models/ApplicationUserModel.cs
@@ -4,6 +4,12 @@ namespace RSA.WebServer.Models
 {
     public class ApplicationUserModel
     {
+        public ApplicationUserModel(string id, string email)
+        {
+            Id = id;
+            Email = email;
+        }
+
         public string Id { get; set; }
         public string Email { get; set; }
 
